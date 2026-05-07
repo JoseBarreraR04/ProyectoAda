325359afde008e878f6e0a8622a55f7876559634
JoseBarrera <chapixjoestar04@gmail.com>
2026-05-06 20:13:53 -0500

    Creacion del main con la entrada de datos y ya

--- fujiko.py
+++ fujiko.py
@@ -1,12 +1,56 @@
 """
 Análisis y Diseño de Algoritmos.
 Jose Barrera Ramos - 8967442
-Problema: C - Robots on Ice
+Proyecto: A - FujikoMine
 """
 
 from sys import stdin, stdout
 
+
+# Key = (nodoActual, numInfecciones)
+memoriaDP = {}
+
+def aux():
+    """
+    :arg:
+        -
+        -
+    :return:
+        -
+    """
+    return 0
+
+def optimizarArbol():
+    """
+    :return:
+    """
+    return 0
+
 def main():
+    """
+    :return:
+        -
+    """
+    for entrada in stdin:
+        entrada = entrada.strip()
+
+        if entrada:
+            #  n            m              q
+            numNodos, numTransmision, numConsultas = map(int, stdin.readline().split())
+
+            Grafo = [[] for _ in range(numNodos)]
+            for _ in range(numNodos - 1):
+                #   u         v      w
+                nodoActual, arista, peso = map(int, stdin.readline().split())
+                Grafo[nodoActual].append((arista, peso))
+                Grafo[arista].append((nodoActual, peso))
+
+            nodosTransmision = list(map(int, stdin.readline().split()))
+            queries = list(map(int, stdin.readline().split()))
+
+            solucion = 0
+            stdout.write(f"{solucion}\n")
+
     return 0
 
 main()
